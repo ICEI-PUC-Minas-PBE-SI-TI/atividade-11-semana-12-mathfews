c3e55cdf88ee68b44466064c42e1601913fcbbee
Matheus Felipe <157317252+mathfews@users.noreply.github.com>
2026-06-10 23:36:23 +0000

    fiz a parte de detalhes dos filmes e o fetch dos atores e diretores

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,21 +1,23 @@
-const card_section = document.querySelector(".card-section");
+const card_section = document.querySelector(".card-section")
+const search_input = document.querySelector(".search-section input")
+const search_button = document.querySelector(".search-section button")
 const api_key = "51bcbad4f2055cf1999bac7cb6ab10de"
 function createMovieCard(obj) {
-    const card = document.createElement("div");
-    card.classList.add("card");
+    const card = document.createElement("div")
+    card.classList.add("card")
     card.innerHTML = `
         <img src="${obj.capa}" alt="${obj.titulo}">
         <p>${obj.titulo}</p>
-    `;
+    `
     card.addEventListener("click", () => {
-        window.location.href = `detalhes.html?id=${obj.id}`;
-    });
-    card_section.appendChild(card);
+        window.location.href = `detalhes.html?id=${obj.id}`
+    })
+    card_section.appendChild(card)
 }
 async function melhoresAvaliados() {
-    const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=pt-BR&page=1`;
-    const response = await fetch(url);
-    const data = await response.json();
+    const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=pt-BR&page=1`
+    const response = await fetch(url)
+    const data = await response.json()
     data.results.forEach((obj) => {
         createMovieCard({
             id: obj.id,
@@ -24,4 +26,38 @@ async function melhoresAvaliados() {
         });
     });
 }
-melhoresAvaliados();
+async function buscarFilmes(query) {
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=pt-BR&query= ${encodeURIComponent(query)}&page=1&include_adult=false`
+    const response = await fetch(url)
+    const data = await response.json()
+    card_section.innerHTML = ""
+    data.results.forEach((obj) => {
+        createMovieCard({
+            id: obj.id,
+            titulo: obj.title,
+            capa: `https://image.tmdb.org/t/p/w500${obj.poster_path}`,
+        });
+    });
+}
+
+search_input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        const query = search_input.value.trim()
+        if (query) {
+            buscarFilmes(query)
+        }
+        if (query === "") {
+            card_section.innerHTML = ""
+            melhoresAvaliados()
+        }
+    }
+})
+
+search_button.addEventListener("click", () => {
+    const query = search_input.value.trim()
+    if (query) {
+        buscarFilmes(query)
+    }
+})
+
+melhoresAvaliados()
--- public/assets/detalhes.js
+++ public/assets/detalhes.js
@@ -1,11 +1,39 @@
+const api_key = "51bcbad4f2055cf1999bac7cb6ab10de"
 const parametros = new URLSearchParams(window.location.search)
-const id = parametros.get("id") - 1
+const id = parametros.get("id")
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-let generos = catalogo[id].generos.map(g => capitalizeFirstLetter(g))
-document.title = `${catalogo[id].titulo} - MOOV`
-document.body.innerHTML = `
+async function getMovieCredits(id) {
+    const url = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${api_key}&language=pt-BR`
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+async function getPersonImage(id) {
+    const url = `https://api.themoviedb.org/3/person/${id}?api_key=${api_key}&language=pt-BR`
+    const response = await fetch(url)
+    const data = await response.json()
+    return `https://image.tmdb.org/t/p/w500${data.profile_path}`
+}
+
+async function getMovie(id) {
+    const movieCredits = await getMovieCredits(id)
+    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&language=pt-BR`
+    const response = await fetch(url)
+    const data = await response.json()
+    const imagePromises = movieCredits.cast.slice(0, 4).map(c => getPersonImage(c.id))
+    const images = await Promise.all(imagePromises)
+    movieCredits.cast.slice(0, 4).forEach((c, index) => {
+        c.foto = images[index]
+    })
+    movieCredits.crew.filter(c => c.job === "Director").forEach(d => {
+        d.tmdb = `https://www.themoviedb.org/person/${d.id}`
+        d.foto = `https://image.tmdb.org/t/p/w500${d.profile_path}`
+    })
+    document.title = `${data.original_title} - FILMS`
+    document.body.innerHTML = `
     <main class="detalhes-wrapper">
         <a href="index.html" class="btn-voltar">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
@@ -14,11 +42,11 @@ document.body.innerHTML = `
             </svg>
             Voltar
         </a>
-        <h1 id="titulo">${catalogo[id].titulo} <span class="ano">${catalogo[id].ano}</span></h1>
+        <h1 id="titulo">${data.original_title} <span class="ano">${data.release_date.slice(0, 4)}</span></h1>
         <div class="container">
             <div class="foto-container">
-                <a href="${catalogo[id].link}" target="_blank">
-                    <img id="capa" src="${catalogo[id].capa}" alt="">
+                <a href="${`https://www.themoviedb.org/movie/${id}`}" target="_blank" onclick="console.log(data.homepage)">)
+                    <img id="capa" src="https://image.tmdb.org/t/p/w500${data.poster_path}" alt="">
                 </a>
             </div>
             <div class="info-container">
@@ -26,26 +54,27 @@ document.body.innerHTML = `
                     <svg id="estrela" class="estrela" viewBox="0 0 24 24" fill="currentColor">
                         <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
                     </svg>
-                    <h1 id="nota">Nota: ${catalogo[id].nota}</h1>
+                    <h1 id="nota">Nota: ${data.vote_average.toFixed(1)}</h1>
                 </div>
                 <ul class="genres">
-                    ${generos.map(g => `<li>${g}</li>`).join("")}
+                    ${data.genres.map(g => `<li>${g.name}</li>`).join("")}
                 </ul>
-                <p id="description">${catalogo[id].descricao}</p>
+                <p id="description">${data.overview}</p>
                 <div class="autores-envolvidos">
                     <div class="diretor">
                         <h2>Diretor</h2>
                         <ul>
-                            ${catalogo[id].diretores.map(d => `<li><a href="${d.tmdb}" target="_blank" class="autor-link"><img src="${d.foto}" alt="">${d.nome}</a></li>`).join("")}
+                            ${movieCredits.crew.filter(c => c.job === "Director").map(d => `<li><a href="${d.tmdb}" target="_blank" class="autor-link"><img src="${d.foto}" alt="">${d.name}</a></li>`).join("")}
                         </ul>
                     </div>
                     <div class="elenco">
                         <h2>Elenco</h2>
                         <ul>
-                            ${catalogo[id].elenco.map(e => `<li><a href="${e.tmdb}" target="_blank" class="autor-link"><img src="${e.foto}" alt="">${e.nome}</a></li>`).join("")}
+                            ${movieCredits.cast.slice(0, 4).map(c => `<li><a href="${`https://www.themoviedb.org/person/${c.id}`}" target="_blank" class="autor-link"><img src="${c.foto}" alt="">${c.name} - ${capitalizeFirstLetter(c.character)}</a></li>`).join("")}
                         </ul>
                     </div>
                 </div>
             </div>
-        </div>
     </main>`
+}
+getMovie(id)
